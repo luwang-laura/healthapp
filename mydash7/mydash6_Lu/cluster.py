6398a065c61907c743f33c41961b40f95a802069
luwang-laura <56236935+luwang-laura@users.noreply.github.com>
2021-09-04 14:21:04 +0100

    Add files via upload

---
+++ mydash7/mydash6_Lu/cluster.py
@@ -0,0 +1,101 @@
+subset_varible=["SEQN","ECD010","ECQ020","ECQ060","ECD070A","MCQ080E","WHQ030E"]  # Filter specified variables
+data=pd.read_csv(input_file_name)
+data1 = data[subset_varible]
+data1 = data1.dropna(axis = 0, how ='any') #Remove Empty data
+data1 = data1[data1.ECD010 <= 60] ; data = data[data.ECQ060 <= 2]
+data1 = data1[data1.ECD070A <= 20] ; data = data[data.WHQ030E <= 3]
+data1 = data1[data1.MCQ080E <= 2] ; data = data[data.ECQ020 <= 2]#Remove abnormal data
+data1.MCQ080E[data1['MCQ080E'] == 2] = 0 #Change value 2 to 0 which means overweight
+print(data1.shape) #Check data dimension
+data1=data1[~data1['ECD010'].isin([999])] # Remove column contains infinity value: 999
+data1.head()
+import sqlite3
+# import cx_Oracle 'username/password@hostname:port/service_name'
+# connect function opens a connection to the SQLite database file,
+conn = sqlite3.connect('database.db')
+#Similarly we will make connection with other databases like Oracle, DB2 etc.
+print(conn)
+# Drop a table name Crypto if it exists already
+try:
+    conn.execute('DROP TABLE IF EXISTS `tan2345` ')
+except Exception as e:
+    raise(e)
+finally:
+    print('Table dropped')
+
+# python check whether table in sqlite exist or not，if not, then create one
+def create_table():
+    conn = sqlite3.connect("database.db")
+    try:
+        create_tb_cmd='''
+       create table tan2345(SEQN integer ,ECD010 integer,ECQ020 integer,ECQ060 integer,WHQ030E integer,ECD070A integer,MCQ080E integer);
+        '''
+        conn.execute(create_tb_cmd)
+    except:
+        print("Create table failed")
+        return False
+    #conn.execute(insert_dt_cmd)
+    conn.commit()
+create_table()
+
+conn = sqlite3.connect("database.db")
+cu=conn.cursor()
+data1.to_sql('tan2345',conn, if_exists='append', index=False)  # Inject the newly read data into the database table data in the form of a data frame
+conn.commit()
+##Read data from database
+conn = sqlite3.connect("database.db")
+#print(conn)
+sql="SELECT * from tan2345"
+newdata=pd.read_sql(sql,conn)
+newdata.head()
+
+num_list = list(newdata['ECQ020'].value_counts()) #获取列ECQ020数据
+#name_list = list(newdata['ECQ020'].value_counts().index) #获取列ECQ020有哪些值
+plt.axes(aspect = 1) #饼图类型
+plt.pie(x = num_list, labels = num_list, autopct = '%3.1f %%') #饼图
+plt.title('ECQ020' + ' Pie Chart') #设置标题
+plt.show()
+count_mean = newdata.mean()['ECD070A']  # 计算整体均值
+mean_result = newdata.groupby('ECD010').mean()['ECD070A']  # 计算不同年龄段均值
+#mean_result = mean_result[mean_result.index <= 35]  # 筛选出年龄小于等于35岁
+x_data = [str(int(i)) for i in mean_result.index] ; y_data = mean_result.values
+
+plt.plot(x_data,y_data, color = 'b', label = 'mean_result') #折线图
+plt.axhline(y = count_mean, color = 'r', label = "Overall the average baby's weight") #添加均值线
+plt.text(38, 6.85, round(count_mean, 2), fontdict = {'size' : '20','color' : 'r'}) #添加值标签
+plt.legend() #图例
+plt.ylabel('ECD070A') ; plt.xlabel('ECD010') #设置x轴，y轴标签
+plt.show()
+
+# Using the elbow method to find the optimal number of clusters
+SSE = []  # Store the sum of squared errors of each result
+for k in range(1, 15):
+    estimator = KMeans(n_clusters = k)  # Construct a clusterer
+    estimator.fit(Kdata)
+    SSE.append(estimator.inertia_)
+X = range(1, 15)
+plt.plot(X, SSE, 'o-')
+#plt.xlabel('k') ; plt.ylabel('SSE')
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('array')
+plt.show()
+kmodel = KMeans(n_clusters = 5) #聚5类
+kmodel.fit(Kdata) #训练模型
+label = pd.Series(kmodel.labels_)  # 各样本的类别
+num = pd.Series(kmodel.labels_).value_counts()  # 统计各样本对应的类别的数目
+center = pd.DataFrame(kmodel.cluster_centers_)  # 找出聚类中心,横变量，纵类别
+Max = center.values.max() ; Min = center.values.min()
+X = pd.concat([center, num], axis = 1)  # 横向连接（0是纵向），得到聚类中心对应的类别数目  <class 'pandas.core.frame.DataFrame'>
+X.columns = list(Kdata.columns) + ['NUM']  # 表头加上一列
+X
+Kdata['Type'] = label.values
+Kdata.head()
+np.ravel(Kdata)
+plt.scatter(Kdata.ECD010[Kdata['Type'] == 0], Kdata.ECD070A[Kdata['Type'] == 0], s = 10, c = 'red', label = '0')
+plt.scatter(Kdata.ECD010[Kdata['Type'] == 1], Kdata.ECD070A[Kdata['Type'] == 1], s = 10, c = 'blue', label = '1')
+plt.scatter(Kdata.ECD010[Kdata['Type'] == 2], Kdata.ECD070A[Kdata['Type'] == 2], s = 10, c = 'green', label = '2')
+plt.scatter(Kdata.ECD010[Kdata['Type'] == 3], Kdata.ECD070A[Kdata['Type'] == 3], s = 10, c = 'cyan', label = '3')
+plt.scatter(Kdata.ECD010[Kdata['Type'] == 4], Kdata.ECD070A[Kdata['Type'] == 4], s = 10, c = 'magenta', label = '4')
+plt.legend()
+plt.show()
---
+++ mydash7/mydash6_Lu/load_data4.py
@@ -0,0 +1,436 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression #Logic Regression Model
+from sklearn import svm
+from sklearn.svm import SVC #SVC
+from sklearn.ensemble import RandomForestClassifier #Random Forest Model
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os as os
+import csv, sqlite3
+import plotly.graph_objects as go
+import copy
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+def clean_data(data):
+    subset_varible=["SEQN","ECD010","ECQ020","ECQ060","ECD070A","MCQ080E","WHQ030E"]  # Filter specified variables
+    #data=pd.read_csv(file_name)
+    data1 = data[subset_varible]
+    data1 = data1[data1.ECD010 <= 60] ; data1 = data1[data1.ECQ060 <= 2]
+    data1 = data1[data1.ECD070A <= 20] ; data1 = data1[data1.WHQ030E <= 3]
+    data1 = data1[data1.MCQ080E <= 2] ; data1 = data1[data1.ECQ020 <= 2]#Remove abnormal data
+    data1.MCQ080E[data1['MCQ080E'] == 2] = 0 #Change value 2 to 0 which means overweight
+    data1 = data1.dropna() # 去除含有空值的行
+    data1.columns = ["ID","Mother's age when born", "Mother smoked when pregnant",
+                   "Receive newborn care at health facility", "Weight at birth, pounds",
+                     "Doctor confirmed overweight", "How do you consider weight"]
+    #print(data1.shape) #Check data dimensions
+    #data1.head()
+    return data1
+
+
+def ETL(data1:pd.DataFrame):
+    conn = sqlite3.connect('database.db')
+    try:
+        conn.execute('DROP TABLE IF EXISTS `tan2345` ')
+    except Exception as e:
+        raise(e)
+    finally:
+        print('Table dropped')
+    def create_table():
+        conn = sqlite3.connect("database.db")
+        try:
+            create_tb_cmd='''
+           create table tan2345(ID integer,Mother's age when born integer,Mother smoked when pregnant integer,Receive newborn care at health facility integer,How do you consider weight integer,Weight at birth, pounds integer,Doctor confirmed overweight integer);
+            '''
+            conn.execute(create_tb_cmd)
+        except:
+            print("Create table failed")
+            return False
+        #conn.execute(insert_dt_cmd)
+        conn.commit()
+    create_table()
+    conn = sqlite3.connect("database.db")
+    cu=conn.cursor()
+    #Insert the newly fetched data into the database's table
+    data1.to_sql('tan2345',conn, if_exists='append', index=False)
+    conn.commit()
+    ##Read the newest data from the database
+    conn = sqlite3.connect("database.db")
+    #print(conn)
+    sql="SELECT * from tan2345"
+    data2=pd.read_sql(sql,conn)
+    return data2
+
+def Pie_data(data2:pd.DataFrame, x_name:str):
+    numbers = list(data2[x_name].value_counts()) #获取列ECQ020数据
+    names = list(data2[x_name].value_counts().index) #获取列ECQ020有哪些值
+    names = ['Mother not smoked when pregnant','Mother smoked when pregnant']
+    pie = {"numbers":numbers, "names":names}
+    return pie
+
+
+def Correlation_data(data2:pd.DataFrame, variables:list):
+    corr = data2[variables].corr(method='kendall')   # Calculate the correlation coefficient of 1 to 3 columns of variables
+    return corr
+
+
+
+def get_heatmap(data_dictx):
+    data_dictx = copy.deepcopy(data_dictx)
+    for k in data_dictx:
+        print(k,data_dictx[k])
+    x = data_dictx['a']
+    y = data_dictx['b']
+    data = data_dictx['data']
+    print(data_dictx)
+
+    #data = [[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20]]
+    fig = px.imshow(data,
+                    #labels=dict(x="Day of Week", y="Time of Day", color="Productivity"),
+                    x=x,
+                    y=y,
+                    )
+    colors = {
+        'background': '#111111',
+        'text': '#7FDBFF'
+    }
+    colors = {
+        'background': '#111111',
+        'text': '#111111'
+    }
+    row1 = html.Div([
+        html.H2(
+            children='HeatMap',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+    ])
+
+    row2 = html.Div([
+        html.Div(
+            children='HeatMap',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+    ])
+    fig.update_layout(
+        # autosize=False,
+        paper_bgcolor="LightSteelBlue",
+    )
+    row3 = html.Div([
+        dcc.Graph(
+            # id='MLreslut',
+            figure=fig
+        )
+    ])
+
+    return [row1, row2, row3]
+
+def get_norm(data):
+    from plotly.subplots import make_subplots
+    import plotly.figure_factory as ff
+    return ff.create_distplot([data['x1']], group_labels=[data['label_1']], show_hist=False)
+    fig = make_subplots(rows=1, cols=2, subplot_titles=('Mother smokes', 'Mother not smokes'))
+    fig.add_trace(
+        ff.create_distplot([data['x1']], group_labels=[data['label_1']],show_hist=False), row=1, col=1
+    )
+
+    fig.add_trace(
+        ff.create_distplot([data['x2']], group_labels=[data['label_2']],show_hist=False),
+        row=1, col=2
+    )
+
+    fig.update_xaxes(title_text="Mother smoked when pregnant", row=1, col=1)
+    fig.update_xaxes(title_text="Mother smoked when pregnant", row=1, col=2)
+
+    fig.update_yaxes(title_text="Weight at birth, pounds", row=1, col=1)
+    fig.update_yaxes(title_text="Weight at birth, pounds", row=1, col=2)
+
+    fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
+    return fig
+
+def Box_data(data2:pd.DataFrame, factor_name: str, x_name:str):
+    factor = data2[factor_name].value_counts().index
+    box = {}
+    for i in factor:
+        box.update({i : list(data2[data2[factor_name] == i][x_name])})
+    return box
+
+def get_pie(pie):
+    fig = go.Figure(data=[go.Pie(labels=pie["names"], values=pie["numbers"])])
+    colors = {
+        'background': '#111111',
+        'text': '#7FDBFF'
+    }
+    colors = {
+        'background': '#111111',
+        'text': '#111111'
+    }
+    row1 = html.Div([
+        html.H2(
+            children='Pie Plot',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+    ])
+
+    row2 = html.Div([
+        html.Div(
+            children='Mother smoked when pregnant',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+    ])
+    fig.update_layout(
+        # autosize=False,
+        paper_bgcolor="LightSteelBlue",
+    )
+    row3 = html.Div([
+        dcc.Graph(
+            # id='MLreslut',
+            figure=fig
+        )
+    ])
+
+    return [row1, row2, row3]
+
+def get_box(data):
+    fig = go.Figure()
+    fig.add_trace(go.Box(y=list(data.values())[0], name="no"))
+    fig.add_trace(go.Box(y=list(data.values())[1], name="yes"))
+    colors = {
+        'background': '#111111',
+        'text': '#7FDBFF'
+    }
+    colors = {
+        'background': '#111111',
+        'text': '#111111'
+    }
+    row1 = html.Div([
+        html.H2(
+            children='Box Plot',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+    ])
+
+    row2 = html.Div([
+        html.Div(
+            children='Receive newborn care at health facility # Weight at birth, pounds',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+    ])
+    fig.update_layout(
+        # autosize=False,
+        paper_bgcolor="LightSteelBlue",
+    )
+    fig.update_yaxes(
+        title_text="Weight at birth, pounds",
+        # title_text="Temperature",
+        # title_standoff=25
+    )
+
+    row3 = html.Div([
+        dcc.Graph(
+            # id='MLreslut',
+            figure=fig
+        )
+    ])
+
+    return [row1, row2, row3]
+
+
+def get_A(data_dict):
+    data_dictx = copy.deepcopy(data_dict)
+    fig = get_heatmap(data_dictx)
+
+
+
+def get_B(pie_data):
+    fig = get_pie(pie_data)
+    colors = {
+        'background': '#111111',
+        'text': '#7FDBFF'
+    }
+    colors = {
+        'background': '#111111',
+        'text': '#111111'
+    }
+    row1 = html.Div([
+        html.H2(
+            children='AUC-ROC curves',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+    ])
+
+    row2 = html.Div([
+        html.Div(
+            children='Mother smoked when pregnant',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+    ])
+    fig.update_layout(
+        # autosize=False,
+        paper_bgcolor="LightSteelBlue",
+    )
+    row3 = html.Div([
+        dcc.Graph(
+            # id='MLreslut',
+            figure=fig
+        )
+    ])
+
+    return [row1, row2, row3]
+
+def get_C(box_data):
+    fig = get_box(box_data)
+    colors = {
+        'background': '#111111',
+        'text': '#7FDBFF'
+    }
+
+    row1 = html.Div([
+        html.H2(
+            children='AUC-ROC curves',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+    ])
+
+    row2 = html.Div([
+        html.Div(
+            children='AUC-ROC curves',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+    ])
+    fig.update_layout(
+        # autosize=False,
+        paper_bgcolor="LightSteelBlue",
+    )
+    row3 = html.Div([
+        dcc.Graph(
+            # id='MLreslut',
+            figure=fig
+        )
+    ])
+
+    return [row1, row2, row3]
+
+def Hist_data(data2:pd.DataFrame, x_name:str):
+    hist = list(data2[x_name])
+    return hist
+
+def get_Hist_data(des_data):
+    # Here we modify the tickangle of the xaxis, resulting in rotated labels.
+    fig = go.Figure(
+        data=[go.Bar(y=des_data)],
+    )
+    fig = px.histogram(np.array(des_data), nbins=10, range_x=[0,20])
+    colors = {
+        'background': '#111111',
+        'text': '#7FDBFF'
+    }
+    #fig.update_layout(bargap=0.2)
+    colors = {
+        'background': '#111111',
+        'text': '#111111'
+    }
+    row1 = html.Div([
+        html.H2(
+            children='Hist',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+    ])
+
+    row2 = html.Div([
+        html.Div(
+            children='Bar Plot',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+    ])
+    fig.update_layout(
+        # autosize=False,
+        paper_bgcolor="LightSteelBlue",
+    )
+
+    fig.update_layout(
+        xaxis_title='Weight at birth, pounds',
+        yaxis_title='Frequency',
+        # yaxis=dict(scaleanchor="x", scaleratio=1),
+        # xaxis=dict(constrain='domain'),
+    )
+
+    row3 = html.Div([
+        dcc.Graph(
+            #id='MLreslut',
+            figure=fig
+        )
+    ])
+
+    return [row1, row2, row3]
+
+def get_data4(file_name=None):
+    #file_name = r"ECQ_D.csv"
+    data1 = clean_data(file_name)
+    data2 = data1
+    data22 = data2.copy()
+    x_pie = "Mother smoked when pregnant"
+    pie_data = Pie_data(data2, x_pie)
+    box_data = Box_data(data2, "Receive newborn care at health facility", "Weight at birth, pounds")
+
+    corr_vars1 = ["Mother's age when born", "Mother smoked when pregnant", "Receive newborn care at health facility",
+                  "Weight at birth, pounds", "Doctor confirmed overweight", "How do you consider weight"]
+    corr1 = Correlation_data(data2, corr_vars1)
+    data_dict = {'data': corr1}
+    data_dict['a'] = corr_vars1
+    data_dict['b'] = corr_vars1
+    x_hist = "Weight at birth, pounds"
+    hist = Hist_data(data22,  x_hist)
+    print("##########################")
+    print(hist)
+    print("#########################")
+    A = get_heatmap(data_dict)
+
+    B = get_pie(pie_data)
+
+    C = get_Hist_data(hist)
+
+    return A,B,C
