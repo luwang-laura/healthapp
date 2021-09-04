c67d2f6b72aa32ae77d0f4244da6d84be349a81a
luwang-laura <56236935+luwang-laura@users.noreply.github.com>
2021-09-04 14:12:56 +0100

    Add files via upload

--- mydash7/load_data1.py
+++ mydash7/load_data1.py
@@ -45,15 +45,18 @@ import dash_bootstrap_components as dbc
 
 def clean_data(data):
     subset_varible=["SEQN","ECD010","ECQ020","ECQ060","ECD070A","MCQ080E","WHQ030E"]  # Filter specified variables
+    #data=pd.read_csv(file_name)
     data1 = data[subset_varible]
     data1 = data1[data1.ECD010 <= 60] ; data1 = data1[data1.ECQ060 <= 2]
     data1 = data1[data1.ECD070A <= 20] ; data1 = data1[data1.WHQ030E <= 3]
     data1 = data1[data1.MCQ080E <= 2] ; data1 = data1[data1.ECQ020 <= 2]#Remove abnormal data
     data1.MCQ080E[data1['MCQ080E'] == 2] = 0 #Change value 2 to 0 which means overweight
-    data1 = data1.dropna() # Remove rows with empty values
+    data1 = data1.dropna() # 去除含有空值的行
     data1.columns = ["ID","Mother's age when born", "Mother smoked when pregnant",
                    "Receive newborn care at health facility", "Weight at birth, pounds",
                      "Doctor confirmed overweight", "How do you consider weight"]
+    #print(data1.shape) #Check data dimensions
+    #data1.head()
     return data1
 
 
@@ -69,11 +72,7 @@ def ETL(data1:pd.DataFrame):
         conn = sqlite3.connect("database.db")
         try:
             create_tb_cmd='''
-           create table tan2345(ID integer,Mother's age when born integer,
-           Mother smoked when pregnant integer,R
-           eceive newborn care at health facility integer,
-           How do you consider weight integer,Weight at birth, pounds integer,
-           Doctor confirmed overweight integer);
+           create table tan2345(ID integer,Mother's age when born integer,Mother smoked when pregnant integer,Receive newborn care at health facility integer,How do you consider weight integer,Weight at birth, pounds integer,Doctor confirmed overweight integer);
             '''
             conn.execute(create_tb_cmd)
         except:
@@ -94,7 +93,7 @@ def ETL(data1:pd.DataFrame):
     data2=pd.read_sql(sql,conn)
     return data2
 
-def Scatter_data(data2:pd.DataFrame, x_name:str, y_name:str, type_name):
+def Scatter_data(data2:pd.DataFrame, x_name:str, y_name:str):
     l = data2.shape[0]
     x = list(data2[x_name].values + np.random.randint(-500, 500, l) / 1000)
     x_model = [[i] for i in x]
@@ -105,22 +104,6 @@ def Scatter_data(data2:pd.DataFrame, x_name:str, y_name:str, type_name):
     min_pre = model.predict([[min(x)]])[0][0]
     max_pre = model.predict([[max(x)]])[0][0]
     scatter = {"x":x, "y":y, "trend_lines":[[min(x), max(x)], [min_pre, max_pre]]}
-
-    x_smoke, y_smoke = [], []
-    type_val = data2[type_name].values
-    for i in range(len(type_val)):
-        if type_val[i] == 1:
-            x_smoke.append(x_model[i])
-            y_smoke.append(y_model[i])
-    model_smoke = linear_model.LinearRegression()
-    model_smoke.fit(x_smoke, y_smoke)
-    min_pre_somke = model_smoke.predict([min(x_smoke)])[0][0]
-    max_pre_smoke = model_smoke.predict([max(x_smoke)])[0][0]
-
-    trend_lines_smoked = {"trend_lines_smoked": [[min(x_smoke)[0], max(x_smoke)[0]], [min_pre_somke, max_pre_smoke]]}
-    scatter = {"x": x, "y": y, "trend_lines": [[min(x), max(x)], [min_pre, max_pre]]}
-    scatter.update(trend_lines_smoked)
-
     return scatter
 
 
@@ -144,17 +127,12 @@ def get_show_scatter(scatter):
     trace1 = go.Scatter(
         x=scatter["trend_lines"][0],
         y=scatter["trend_lines"][1],
-        name='all_mean'
+        name='mean'
     )
 
-    trace2 = go.Scatter(
-        x=scatter["trend_lines_smoked"][0],
-        y=scatter["trend_lines_smoked"][1],
-        name='smoke_mean'
-    )
 
     fig = go.Figure(
-        data = [trace,trace1,trace2],
+        data = [trace,trace1],
     )
     colors = {
         'background': '#111111',
@@ -183,7 +161,7 @@ def get_show_scatter(scatter):
 
     row1 = html.Div([
         html.H2(
-            children='Mean and Distribution',
+            children='The Average',
             style={
                 'textAlign': 'center',
                 'color': colors['text']
@@ -193,7 +171,7 @@ def get_show_scatter(scatter):
 
 
     row2 = html.Div([
-        html.Div(children='The red line shows the average weight change with mother\'s age.'+ '\nThe green line shows the average weight of babies of smoking mothers is lower in comparision.', style={
+        html.Div(children='The red line shows the average weight change with mother\'s age.', style={
             'textAlign': 'center',
             'color': colors['text']
         }),
@@ -213,7 +191,7 @@ def get_heatmap(data_dict):
     data = data_dict['data']
     import plotly.express as px
 
-    label_list =["Mother's age",'Mother smoked',"Baby's weight"]
+    label_list =['MAP','MSP','WBP']
 
 
     #data = [[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20]]
@@ -249,40 +227,22 @@ def get_heatmap(data_dict):
 
     row1 = html.Div([
         html.H2(
-            children='The Association Hypothesis',
+            children='The Association',
             style={
                 'textAlign': 'center',
                 'color': colors['text']
             }
         ),
     ])
-    # row2 = html.Div([
-    #     html.Div(
-    #         children=x_str,
-    #         style={
-    #             'textAlign': 'left',
-    #             'color': colors['text']
-    #         })
-    #     for x_str in "MAP: Mother\'s age when born. @#MSP: Mother smoked when pregnant. @#WBP: Weight at birth, pounds".split("@#")
-    # ])
-    
+
     row2 = html.Div([
         html.Div(
-            children="It seems that the mother’s age at childbirth, whether the mother smoked during pregnancy, and the weight of the newborn baby are weakly correlated",
+            children='MAP: Mother\'s age when born. MSP: Mother smoked when pregnant. WBP: Weight at birth, pounds',
             style={
                 'textAlign': 'center',
                 'color': colors['text']
             }),
     ])
-    #     row2 = html.Div([
-    #     html.Div(
-    #         children=x_str,
-    #         style={
-    #             'textAlign': 'left',
-    #             'color': colors['text']
-    #         })
-    #     for x_str in "MAP: Mother\'s age when born. @#MSP: Mother smoked when pregnant. @#WBP: Weight at birth, pounds".split("@#")
-    # ])
     return [row1,row2,row3]
 
 
@@ -299,26 +259,25 @@ def Test(data2, x_name, y_name, x_type, y_type, groups, normal=''):
             nums0 = nums[cate == cate_types[0]]
             nums1 = nums[cate == cate_types[1]]
             t, p_two, df = st.ttest_ind(nums0, nums1)
-            words = 'T test：' + 't=' + str(round(t,2)) + ',P value=' + str(round(p_two,2)) + ',Freedom degree=' + str(round(df,2))
+            words = '\nT-test: \n' + 't=' + str(round(t,2))  + ',P-value=' + str(round(p_two,2)) + ',freedom degree=' + str(round(df,2)) + '\n'
             alpha = 0.05
             if (p_two < alpha):
-                words = words + '\nReject hypothesis, There is a significant difference in the weight of newborn babies whose mothers smoked during pregnancy and those whose mothers did not smoke during pregnancy.'
-            #    words = words + '\nP<α，%s has significant difference with %s %s' % \
-            #             (cate.name + str(cate_types[0]), cate.name + str(cate_types[1]), nums.name)
+                words = '\n'+ words + 'P<α，the average value of %s and %s%s has significantly difference ' % \
+                        (cate.name + str(cate_types[0]), cate.name + str(cate_types[1]), nums.name) + '\n'
             else:
-               words = words + '\nReject hypothesis，%s has no significant difference with %s %s' % \
-                        (cate.name + str(cate_types[0]), cate.name + str(cate_types[1]), nums.name)
+                words = '\n'+words + 'P>α，the average value of %s and %s%s has no significant difference ' % \
+                        (cate.name + str(cate_types[0]), cate.name + str(cate_types[1]), nums.name) + '\n'
 
         elif len(data2[x_name].value_counts().index) < 5 and len(data2[y_name].value_counts().index) < 5:
             # 说明有2个变量都为Categrical;卡方检验
             table = pd.crosstab(data2[x_name], data2[y_name])
             chi = chi2_contingency(table)
-            words = 'Chi test：' + 'chi=' + str(round(chi[0],2)) + ',P value=' + str(round(chi[1],2)) + ',Freedom degree=' + str(round(chi[2],2))
+            words = '\nChe-test: \n' + 'chisq-statistic=' + str(round(chi[0],2)) + ',P-value=' + str(round(chi[1],2)) + ',freedom degree=' + str(round(chi[2],2)) + '\n'
             alpha = 0.05
             if (chi[1] < alpha):
-               words = words + '\nAccept hypothesis，%s has significant difference with %s.' % (x_name, y_name)
+                words = '\n' + words + 'P<α,%s and %s has significant difference' % (x_name, y_name) + '\n'
             else:
-                  words = words + '\nAccept hypothesis，%s has no significant difference with %s.' % (x_name, y_name)
+                words = '\n' + words + 'P>α，%s and %s has no significant difference'  % (x_name, y_name) + '\n'
         elif x_type + y_type == 1 and groups == 1:
             # 说明有1个变量为Categrical;方差检验
             cate = data2[x_name] if len(data2[x_name].value_counts().index) < \
@@ -329,18 +288,16 @@ def Test(data2, x_name, y_name, x_type, y_type, groups, normal=''):
             for i in cate.value_counts().index:
                 aov.append(nums[cate == i])
             aov_test = stats.f_oneway(*aov)
-            words = 'Suggest to use： T-test  t-value =-3.702, p-value=0.00022'+ "\nReject hypothesis, There is a significant difference of the baby's weight between smoking mothers and non-smoking mothers during pregnancy."
-            # + 'F=' + str(round(aov_test[0],2)) + ',P value=' + str(round(aov_test[1],2)) +"\nReject hypothesis, There is a significant difference of the baby's weight between smoking mothers and non-smoking mothers during pregnancy."
+            words = '\nVariance test：\n' + 'F=' + str(round(aov_test[0],2)) + ',P-value=' + str(round(aov_test[1],2)) + '\n'
             alpha = 0.05
             if (aov_test[1] < alpha):
-                 words = words +'  '
-                            #  words = words + '\nReject hypothesis，%s has significant difference with %s.' % (cate.name, nums.name)
+                words = words + '\nP<α，%s has significant difference with %s.\n' % (cate.name, nums.name) + '\n'
             else:
-                words = words + '\nAccept hypothesis，%s has no significant difference with %s.' % (cate.name, nums.name)
+                words = words + '\nP>α，%s has no significant difference with %s.\n' % (cate.name, nums.name) + '\n'
         else:
-            words = 'Error, Please re-select'
+            words = 'error,please re-select\n'
     except:
-        words = 'Error, Please re-select'
+        words = 'error,please re-select\n'
 
     return words
 
@@ -361,7 +318,6 @@ def get_norm(data, all_result):
     fig.update_layout(
         #autosize=False,
         paper_bgcolor="LightSteelBlue",
-        #paper_bgcolor=["Blue","Yellow","Green"]
     )
 
     colors = {
@@ -369,23 +325,9 @@ def get_norm(data, all_result):
         'text': '#7FDBFF'
     }
     colors = {
-        'background': '#2d3035',
+        'background': '#111111',
         'text': '#111111'
     }
-
-    fig.update_xaxes(
-        # tickangle=90,
-        title_text="weight at birth, pounds",
-        # title_font={"size": 20},
-        # title_standoff=25
-    )
-
-    fig.update_yaxes(
-        title_text="Density",
-        # title_text="Temperature",
-        # title_standoff=25
-    )
-
     #fig.update_layout(l=50, r=50, t=50, b=50, title_text="Side By Side Subplots")
     row1 = html.Div([
         html.H2(
@@ -399,12 +341,11 @@ def get_norm(data, all_result):
 
     row2 = html.Div([
         html.Div(
-            children=x_str,
+            children=all_result,
             style={
-                'textAlign': 'left',
+                'textAlign': 'center',
                 'color': colors['text']
-            })
-        for x_str in all_result.split("@#")
+            }),
     ])
 
     row3 = html.Div([
@@ -423,11 +364,9 @@ def get_data1(file_name):
     #data2 = ETL(data1)
     x_scatter = "Mother's age when born"
     y_scatter = "Weight at birth, pounds"
-    type_name = "Mother smoked when pregnant"
-    scatter = Scatter_data(data2, x_scatter, y_scatter, type_name)
+    scatter = Scatter_data(data2, x_scatter, y_scatter)
     scatter['x_label'] = x_scatter
     scatter['y_label'] = y_scatter
-    scatter['x_label'] = type_name
 
     data1.columns = ["ID", "Mother's age when born", "Mother smoked when pregnant",
                      "Receive newborn care at health facility", "Weight at birth, pounds",
@@ -442,22 +381,22 @@ def get_data1(file_name):
     norm_data = {}
     factor_name = 'Mother smoked when pregnant';
     x_name = 'Weight at birth, pounds'
-    smoke = data2[data2[factor_name] == 1][x_name]  # 母亲吸烟婴儿体重
-    nosmoke = data2[data2[factor_name] == 2][x_name]  # 母亲不吸烟婴儿体重
+    smoke = data2[data2[factor_name] == 1][x_name]  # Mother smoking baby weight
+    nosmoke = data2[data2[factor_name] == 2][x_name]  # Mother not smoking baby weight
     x1 = smoke.values.tolist()
     x2 = nosmoke.values.tolist()
-    norm_data['label_1'] = "Mother smoked"
-    norm_data['label_2'] = "Mother not smoked"
+    norm_data['label_1'] = "Mother smokes"
+    norm_data['label_2'] = "Mother not smokes"
     norm_data['x1'] = x1
     norm_data['x2'] = x2
 
-    aov_test = Test(data2, 'How do you consider weight', 'Weight at birth, pounds', 1, 0, 1, normal=True)  # 方差检验
+    aov_test = Test(data2, 'How do you consider weight', 'Weight at birth, pounds', 1, 0, 1, normal=True)  # Variance test
     print(aov_test)
-    t_test = Test(data2, 'Mother smoked when pregnant', 'Weight at birth, pounds', 1, 0, 0, normal=True)  # T检验
+    t_test = Test(data2, 'Mother smoked when pregnant', 'Weight at birth, pounds', 1, 0, 0, normal=True)  # T test
     print(t_test)
-    chi_test = Test(data2, 'Mother smoked when pregnant', 'Doctor confirmed overweight', 1, 1, 2)  # 卡方检验
+    chi_test = Test(data2, 'Mother smoked when pregnant', 'Doctor confirmed overweight', 1, 1, 2)  # Che test
     print(chi_test)
-    all_result = "@#@#".join([aov_test, t_test, chi_test]).replace("\n", "@#")
+    all_result = "\n".join([aov_test, t_test, chi_test])
     return get_show_scatter(scatter),get_heatmap(data_dict),get_norm(norm_data, all_result)
 
 
@@ -480,8 +419,8 @@ if __name__=="__main__":
 
     norm_data = {}
     factor_name = 'Mother smoked when pregnant'; x_name = 'Weight at birth, pounds'
-    smoke = data2[data2[factor_name]==1][x_name] #母亲吸烟婴儿体重
-    nosmoke = data2[data2[factor_name]==2][x_name] #母亲不吸烟婴儿体重
+    smoke = data2[data2[factor_name]==1][x_name] #Mother smoking baby weight
+    nosmoke = data2[data2[factor_name]==2][x_name] #Mother not smoking baby weight
     x1 = smoke.values.tolist()
     x2 = nosmoke.values.tolist()
     norm_data['label_1'] = "x"
